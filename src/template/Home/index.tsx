257db2791e9fad4a14b38656af4822e06e57467c
Matheus Santos <matheus.santos@polijunior.com.br>
2024-08-03 00:07:37 -0300

    Feat/ Home screen

--- src/template/Home/index.tsx
+++ src/template/Home/index.tsx
@@ -1,13 +1,68 @@
-import Test from 'components/TestComponent';
-import * as S from './styles';
+import React from 'react';
+import { Layout, Menu, Card } from 'antd';
+import {
+  HomeOutlined,
+  BarChartOutlined,
+  AppstoreOutlined,
+  CalendarOutlined,
+  PlusOutlined,
+} from '@ant-design/icons';
 
-const HomeTemplate = () => (
-    <S.Container>
-        <S.Title>
-            Esse template foi criado pela equipe de Research 21.1 para projetos
-            internos da Polijunior.
-        </S.Title>
-        <Test title="Esse componente é um teste" />
-    </S.Container>
-);
-export default HomeTemplate;
+const { Header, Content, Sider } = Layout;
+
+const AdminDashboard: React.FC = () => {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider style={{background: '#FCFCFC'}}>
+        <div className="logo" style={{ padding: '20px', color: 'black', fontWeight: 'bold', fontSize: '18px', background: '#F8C687' }}>
+          Horto Club
+        </div>
+        <Menu style={{background: '#F6F6F6'}} mode="inline">
+          <Menu.Item key="1" icon={<HomeOutlined />}>
+            Home
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0, background: 'linear-gradient(90deg, #F8C687 0%, #CC8D3E 100%)', }} />
+        <Content style={{ margin: '16px' }}>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <h2>Área do Administrador</h2>
+            <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+              <Card
+                hoverable
+                style={{ width: 240, textAlign: 'center' }}
+                cover={<BarChartOutlined style={{ fontSize: '64px', margin: '16px 0', color: '#c47f38' }} />}
+              >
+                <Card.Meta title="Análise de Usuário" />
+              </Card>
+              <Card
+                hoverable
+                style={{ width: 240, textAlign: 'center' }}
+                cover={<AppstoreOutlined style={{ fontSize: '64px', margin: '16px 0', color: '#c47f38' }} />}
+              >
+                <Card.Meta title="Gerenciar Empresas" />
+              </Card>
+              <Card
+                hoverable
+                style={{ width: 240, textAlign: 'center' }}
+                cover={<CalendarOutlined style={{ fontSize: '64px', margin: '16px 0', color: '#c47f38' }} />}
+              >
+                <Card.Meta title="Gerenciar Eventos" />
+              </Card>
+              <Card
+                hoverable
+                style={{ width: 240, textAlign: 'center' }}
+                cover={<PlusOutlined style={{ fontSize: '64px', margin: '16px 0', color: '#c47f38' }} />}
+              >
+                <Card.Meta title="Gerenciar Posts" />
+              </Card>
+            </div>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default AdminDashboard;
